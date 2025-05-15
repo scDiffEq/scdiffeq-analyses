43da958b7ded6f58e1808a86437a7473e70010c2
Michael Vinyard <mvinyard.ai@gmail.com>
2025-05-15 10:28:11 -0600

    undo erroneous tracked changes from dev

--- scdiffeq_analyses/__init__.py
+++ scdiffeq_analyses/__init__.py
@@ -1,6 +1,5 @@
 from . import parsers
 from . import _plotting as pl
 from . import _tools as tl
-from . import hp_scan
 
-__all__ = ["parsers", "pl", "tl", "hp_scan"]
+__all__ = ["parsers", "pl", "tl"]
