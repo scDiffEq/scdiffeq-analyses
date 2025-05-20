4ae79fba722a8146876c247d049177089c85ea54
Michael Vinyard <mvinyard.ai@gmail.com>
2025-05-20 14:18:01 -0600

    rm old

--- scdiffeq_analyses/__init__.py
+++ scdiffeq_analyses/__init__.py
@@ -3,6 +3,11 @@ from .__version__ import __version__
 from . import parsers
 from . import _plotting as pl
 from . import _tools as tl
+<<<<<<< HEAD
 from . import metrics
 
 __all__ = ["parsers", "metrics", "pl", "tl", "__version__"]
+=======
+
+__all__ = ["parsers", "pl", "tl"]
+>>>>>>> 43da958 (undo erroneous tracked changes from dev)
